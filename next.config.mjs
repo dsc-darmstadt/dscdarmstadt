696f38423776773fbaf98104cc3f8cae7be623fb
Adham Elaraby <adhamramy0@gmail.com>
2025-05-26 02:34:32 +0200

    downgrade nextjs, fix errors move to tailwindcss v3 from v4

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,28 @@
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 import createNextIntlPlugin from 'next-intl/plugin';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  // output: 'export',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: { unoptimized: true },
+  productionBrowserSourceMaps: false,
+  optimizeFonts: false,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname),
+    };
+    return config;
+  },
 };
 
 if (process.env.NODE_ENV === 'development') {
