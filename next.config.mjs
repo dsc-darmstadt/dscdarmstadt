897205f8eb21661b460852cddd30c2a171e668cd
Adham Elaraby <adhamramy0@gmail.com>
2025-05-26 02:05:56 +0200

    support cloudflare pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
-import type { NextConfig } from "next";
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   /* config options here */
 };
+
+if (process.env.NODE_ENV === 'development') {
+   await setupDevPlatform();
+ }
 
 export default withNextIntl(nextConfig);
